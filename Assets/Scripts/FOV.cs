79dcc7cecfbdb3905e6541eafb8ea13fd0381d3d
Jaseem P <jaseemp718@gmail.com>
2025-10-13 23:20:20 +0530

    made a visual cone (FOV) for enemy

---
+++ Assets/Scripts/FOV.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+    [SerializeField] GameObject playerRef;
+    [SerializeField]LayerMask targetMask;
+    [SerializeField]LayerMask obstructionMask;
+    [SerializeField] float radius;
+    [SerializeField] float angle;
+
+    bool isPlayerDetected;
+
+    void Start()
+    {
+        StartCoroutine(FieldOfView());
+
+    }
+
+   
+    private IEnumerator FieldOfView()
+    {
+        //Debug.Log("Coroutine running!");
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+        while (true)
+        {
+            yield return wait;
+            CheckFOV();
+        }
+    }
+    
+    private void CheckFOV()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+            float angleToTarget = Vector3.Angle(transform.forward, directionToTarget);
+            Debug.DrawLine(transform.position, directionToTarget);
+            //Debug.Log("Angle to player : "+angleToTarget);
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                {
+                    Debug.DrawLine(transform.position, directionToTarget);
+                    isPlayerDetected = true;
+                    Debug.Log("Player spotted!");
+
+                }
+                else
+                {
+                    isPlayerDetected = false;
+                }
+            }
+            else
+            {
+                isPlayerDetected = false;
+            }
+        }
+        else
+        {
+            isPlayerDetected = false;
+        } 
+    }
+
+}
